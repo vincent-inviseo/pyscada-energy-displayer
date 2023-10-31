cfafa975a690429dae195f0e53e936d7ea076899
vincent agi <vincent@inviseo.fr>
2023-10-31 08:25:33 +0100

    modify css and js style

--- pyscada/energydisplayer/static/pyscada/js/energy-displayer.js
+++ pyscada/energydisplayer/static/pyscada/js/energy-displayer.js
@@ -7,13 +7,11 @@ function update_energy_displayer_value(e) {
     data_handler_ajax(1,[energy_widget_variable_id,],[], e.detail.picker.startDate.valueOf());
     if(energy_widget_variable_id !== undefined && energy_widget_variable_id in DATA) {
         index_start = find_index_sub_lte(DATA[energy_widget_variable_id],e.detail.picker.startDate.valueOf(),0);
-        console.log("index start ", index_start, e.detail.picker.startDate.valueOf());
         index_end = find_index_sub_lte(DATA[energy_widget_variable_id],e.detail.picker.endDate.valueOf(),0);
-        console.log("index_end", index_end, e.detail.picker.endDate.valueOf());
         value_of_start_index = DATA[energy_widget_variable_id][index_start][1]
         value_of_end_index = DATA[energy_widget_variable_id][index_end][1]
-        value_computed = value_of_end_index - value_of_start_index
-        value_array = value_computed.split('.')
+        value_of_end_index - value_of_start_index
+        value_array = value_computed.toString().split('.')
         value = value_array[0] + "." + value_array[1].substring(0, 2)
         e.target.innerHTML = value
     }

b31af13b6b56ddeed66b3db9efe89d7c04ec2c85
vincent agi <vincent@inviseo.fr>
2023-11-06 09:23:10 +0100

    change string any conversion

--- pyscada/energydisplayer/static/pyscada/js/energy-displayer.js
+++ pyscada/energydisplayer/static/pyscada/js/energy-displayer.js
@@ -11,7 +11,7 @@ function update_energy_displayer_value(e) {
         value_of_start_index = DATA[energy_widget_variable_id][index_start][1]
         value_of_end_index = DATA[energy_widget_variable_id][index_end][1]
         value_of_end_index - value_of_start_index
-        value_array = String(value_computed).split('.')
+        value_array = value_computed
         value = value_array[0] + "." + value_array[1].substring(0, 2)
         e.target.innerHTML = value
     }
